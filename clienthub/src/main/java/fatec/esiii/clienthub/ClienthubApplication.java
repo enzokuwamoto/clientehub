5efa9bc43ed5260ac2b3f15f824f6f1b715a3eed
ENZO YUDI KUWAMOTO YANO <enzoyano@suzano.com.br>
2026-05-06 22:12:49 -0300

    first commit

---
+++ clienthub/src/main/java/fatec/esiii/clienthub/ClienthubApplication.java
@@ -0,0 +1,13 @@
+package fatec.esiii.clienthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienthubApplication.class, args);
+	}
+
+}
---
+++ clienthub/src/test/java/fatec/esiii/clienthub/ClienthubApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.esiii.clienthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
